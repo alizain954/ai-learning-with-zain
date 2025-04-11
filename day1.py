7237e2d69d2a610598148a90ce1a7a44731af04f
aws_cc_alizain-at-700792775720 <ali.zain@epazz.net>
2025-04-11 12:50:39 +0500

    this is my firstday code

---
+++ day1.py
@@ -0,0 +1,17 @@
+# First variable
+# variable means if you store the value in any name e.g
+name = 'zain'
+# you store the zain is varible of name 
+# varible = value
+
+age = 27
+weight = 82.2
+is_muslim = True
+
+# thers is also used the data type name is in string and age is in int , weight is in float(decimanl), and is_muslims is boolean
+# we show the output with the function is print function
+
+print(name)
+print(age)
+print(weight)
+print(is_muslim)
