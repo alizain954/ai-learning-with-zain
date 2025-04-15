2bec8a92d1781b77294ab0d4b681f5fdbd1a21c2
aws_cc_alizain-at-700792775720 <ali.zain@epazz.net>
2025-04-15 13:56:09 +0500

    day4 lamdba map etc

--- day4.py
+++ day4.py
@@ -35,3 +35,33 @@ def cal_factorial(num):
     return result
 result = cal_factorial(5)
 print(result)
+
+
+#lambda Function 
+# the function which have no name 
+show = lambda x : x+5
+
+print(show(5))
+
+# Assigment You are given a list of integers. Square each number using map() and lambda.
+
+numbers = [1, 2, 3, 4, 5]
+# Expected Output: [1, 4, 9, 16, 25]
+
+a = list(map(lambda x:x**2, numbers))
+print(a)
+
+# Assigmet 2 From a list of integers, filter out only even numbers.
+numbers = [10, 15, 20, 25, 30, 35]
+# Expected Output: [10, 20, 30]
+
+b = list(filter(lambda x: x%10 ==0, numbers))
+print(b)
+
+# Assigment 3.Given a list of numbers, use reduce() to calculate the product of all elements.
+
+numbers = [1,2,3,4,5]
+from functools import reduce
+
+d = reduce(lambda x, y:x*y, numbers)
+print(d)
